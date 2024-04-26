e53b74559604b2554bc2b59fd6f4264e53fb239a
Earnest Achayo <earnytechlive@gmail.com>
2024-04-26 22:23:32 +0300

    pre-hook test

---
+++ main.py
@@ -0,0 +1,7 @@
+# #hello_world!s
+
+[default]
+aws_access_key_id = AHAKSKSJSKLKSLKSJKDJDHUHSUBUSB
+aws_secret_access_key = KjdjnnjnjnTbdsbshxkkmckTVHVAahsvahsahvahdvahdvadAb
+output = json
+region = us-east-2
